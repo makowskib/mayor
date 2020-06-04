aeadce86d0ad31dc55d8bdcd6161c00d3a65141f
Alex <cushmanalex@gmail.com>
2020-06-04 16:02:08 -0400

    split message page and recipients page into seperate routes

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'data.sqlite')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+THREADS_PER_PAGE = 2
+
+CSRF_ENABLED = True
+CSRF_SESSION_KEY = 'secret'
+CSRF_SECRET = 'secret'
--- mayors/routes.py
+++ mayors/routes.py
@@ -1,10 +1,14 @@
 from mayors import app, db
-from flask import render_template, url_for
+from flask import render_template, url_for, redirect
 from mayors.models import Mayors
 
 
 @app.route("/")
 def index():
+    return render_template("index.html")
+
+@app.route("/recipients")
+def recipients():
     results = Mayors.query.all()
-    return render_template("index.html", results=results)
+    return render_template("recipients.html", results=results)
 
