d6335076fa09a5d68c72bd3d90900f98c3e368f6
Alex <cushmanalex@gmail.com>
2020-06-04 11:36:23 -0400

    even more editing, updating jQuery and css

--- mayors/__init__.py
+++ mayors/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.static_folder = 'static'
 
 db = SQLAlchemy(app)
+bootstrap = Bootstrap(app)
 
 from mayors import routes
--- mayors/routes.py
+++ mayors/routes.py
@@ -1,10 +1,10 @@
 from mayors import app, db
-from flask import render_template
+from flask import render_template, url_for
 from mayors.models import Mayors
 
 
 @app.route("/")
-def hello():
-    db.create_all()
-    return render_template('index.html')
+def index():
+    results = Mayors.query.all()
+    return render_template("index.html", results=results)
 
