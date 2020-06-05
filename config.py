ebf550f397b5f74101eb0306330f14451f410aae
Alex <cushmanalex@gmail.com>
2020-06-04 20:29:25 -0400

    test sendgrid api for mail (i'm so bad w/ git)

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
+from dotenv import load_dotenv
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+load_dotenv()
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'data.sqlite')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -9,3 +11,5 @@ THREADS_PER_PAGE = 2
 CSRF_ENABLED = True
 CSRF_SESSION_KEY = 'secret'
 CSRF_SECRET = 'secret'
+
+SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
--- mayors/routes.py
+++ mayors/routes.py
@@ -1,7 +1,9 @@
+import smtplib, email, sendgrid, os
 from mayors import app, db
 from flask import render_template, url_for
 from mayors.models import Mayors
-
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 @app.route("/")
 def index():
@@ -12,3 +14,18 @@ def index():
 def recipients():
     results = Mayors.query.all()
     return render_template("recipients.html", results=results)
+
+@app.route("/mailtest")
+def mailtest():
+    message = Mail(
+        from_email='cushmanalex@gmail.com',
+        to_emails='to@example.com',
+        subject='Sending with Twilio SendGrid is Fun',
+        html_content='<strong>and easy to do anywhere, even with Python</strong>')
+    sg = SendGridAPIClient(app.config.get('SENDGRID_API_KEY'))
+    response = sg.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+    return render_template('index.html')
+
